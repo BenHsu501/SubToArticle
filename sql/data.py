8261f178ac85953edea3e91719e74149beb646aa
BenHsu501 <stat0v9456reg@gmail.com>
2024-06-09 17:25:30 +0800

    Add database file

---
+++ sql/data.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+try:
+    # 连接到 SQLite 数据库
+    conn = sqlite3.connect('sql/yt_info.db')
+    print("数据库连接成功，版本为", sqlite3.version)
+
+    # 创建一个 cursor 对象
+    c = conn.cursor()
+
+    # 确认数据表存在
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = c.fetchall()
+    print("数据库中的表：", tables)
+
+    # 尝试插入数据
+    video = ("test_video_id", "Test Video", "http://youtube.com/test", "Test description", 100, 150, "http://youtube.com/test", "youtube.com", "youtube", "Test Playlist", "TPL", "Test Uploader", "TU1", 1, "10:00", "20220101", "No", "No", "No")
+    c.execute('''
+    INSERT OR REPLACE INTO videos (id, title, url, description, duration, view_count, webpage_url, webpage_url_domain, extractor, playlist_title, playlist_id, playlist_uploader, playlist_uploader_id, n_entries, duration_string, upload_date, has_subtitles, has_generated_article, has_uploaded_article)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', video)
+
+    # 提交事务
+    conn.commit()
+    print("数据插入并提交成功")
+
+    # 查询数据
+    c.execute("SELECT * FROM videos WHERE id = 'test_video_id';")
+    results = c.fetchall()
+    print("查询到的数据：", results)
+
+except sqlite3.Error as error:
+    print("数据库错误：", error)
+
+finally:
+    if conn:
+        conn.close()
+        print("数据库连接已关闭")
---
+++ sql/schema.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+# 连接到 SQLite 数据库
+# 如果数据库不存在，会自动创建一个数据库文件
+conn = sqlite3.connect('./sql/yt_info.db')
+
+# 创建一个 cursor 对象
+c = conn.cursor()
+
+# 创建表
+c.execute('''
+CREATE TABLE videos (
+    id TEXT PRIMARY KEY,
+    title TEXT,
+    url TEXT,
+    description TEXT,
+    duration INTEGER,
+    view_count INTEGER,
+    webpage_url TEXT,
+    webpage_url_domain TEXT,
+    extractor TEXT,
+    playlist_title TEXT,
+    playlist_id TEXT,
+    playlist_uploader TEXT,
+    playlist_uploader_id TEXT,
+    n_entries INTEGER,
+    duration_string TEXT,
+    upload_date TEXT,
+    has_subtitles TEXT DEFAULT 'No',
+    has_generated_article TEXT DEFAULT 'No',
+    has_uploaded_article TEXT DEFAULT 'No'
+);
+''')
+
+# 提交事务
+conn.commit()
+
+# 关闭连接
+conn.close()
