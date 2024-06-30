42530cddefc98b8acbf3b9ffb3d4bd02999f187c
BenHsu501 <stat0v9456reg@gmail.com>
2024-06-30 19:54:08 +0800

    * feat(subtitle_downloader.py): add support for downloading audio and transcribing it * feat(subtitle_downloader.py): add support for downloading multiple subtitles at once

--- core/subtitle_downloader.py
+++ core/subtitle_downloader.py
@@ -1,11 +1,20 @@
 from core.utils import  OperateDB, MediaDownloader, WhisperRecognizer
+from typing import List
 
 class MediaOperations:
-    def __init__(self, channel_url: str, output_dir: str = 'output/', mode: str = ''):
+    def __init__(self, channel_url: str, output_dir: str = 'output/', download_mode: str = 'mp3'):
         self.channel_url = channel_url
         self.output_dir = output_dir
+        self.download_mode = download_mode
 
-    def download_single_subtitles(self, video_id:str, download_mode:str = 'mp3'):
+    def download_audio_and_transcribe(self, video_id: str):
+        downloader = MediaDownloader()
+        downloader.downlaod_audio(video_id=video_id, download_type='mp3')
+        client = WhisperRecognizer()
+        result = client.transcribe_audio(video_id)
+        return result
+
+    def download_single_subtitles(self, video_id:str, download_mode:str = None):
         downloader = MediaDownloader()
         state_result = None
 
@@ -13,14 +22,16 @@ class MediaOperations:
             state_result = downloader.check_and_download_subtitles(video_id, 0)
             
             if download_mode == 'both' and state_result['state'] in ['NotFound' in 'Error']:
-                downloader.downlaod_audio(video_id = video_id, download_type = 'mp3')
-                client = WhisperRecognizer()
-                result = client.transcribe_audio(video_id)
-        if download_mode == 'mp3':
-            downloader.downlaod_audio(video_id = video_id, download_type = 'mp3')
-            client = WhisperRecognizer()
-            # recognizer and save result
-            result = client.transcribe_audio(video_id)
+                result = self.download_audio_and_transcribe(video_id)
 
-    def download_subtitles(self, ):
-        1
+        if download_mode == 'mp3':
+            result = self.download_audio_and_transcribe(video_id)
+
+    def download_subtitles(self, video_ids:List):
+        if not isinstance(video_ids, list):
+            video_ids = [video_ids]
+        for video_id in video_ids:
+            self.download_single_subtitles(video_id, self.download_mode)
+            
+
+
